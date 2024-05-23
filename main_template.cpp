9d9131e39d3bbcd19c94a3e65c2cd152e78790bd
Luverduck <hyekko92@gmail.com>
2024-05-23 21:04:49 +0900

    initial commit

---
+++ main_template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+
+    return 0;
+}
