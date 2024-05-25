73ce459aa55b9b5609b1d44a1ea47661949c01c2
Luverduck <hyekko92@gmail.com>
2024-05-25 22:18:45 +0900

    [11022번] A+B -8

---
+++ 01_by_level/03_loop/08_11022.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    int a, b;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+    }
+};
