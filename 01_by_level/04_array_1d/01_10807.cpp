b3ad3ab352003beba849d7e4d3eaf1530c60dd60
Luverduck <hyekko92@gmail.com>
2024-05-26 03:10:50 +0900

    [10807번] 개수 세기

--- 01_by_level/04_array_1d/01_10807.cpp
+++ 01_by_level/04_array_1d/01_10807.cpp
@@ -7,19 +7,17 @@ int main()
 #endif
 
     int n, target;
-    int count = 0;
+    int arr[201] = {0};
 
     scanf("%d", &n);
 
-    int arr[n];
-
-    for(int i = 0; i < n; i++)
-        scanf("%d", arr + i);
+    for(int i=0;i<n;i++){
+        int tmp;
+        scanf("%d", &tmp);
+        arr[tmp + 100]++;
+    }
 
     scanf("%d", &target);
 
-    for(int i = 0; i < n; i++)
-        if(arr[i] == target) count++;
-
-    printf("%d", count);
+    printf("%d", arr[target + 100]);
 };
