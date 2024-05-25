1b7401e2c7719c291f8bd9282ef9ff63fe70990e
Luverduck <hyekko92@gmail.com>
2024-05-25 17:02:20 +0900

    A-B

---
+++ 01_by_level/lv_01/03_1001.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d", a - b);
+
+    return 0;
+};
