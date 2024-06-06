7d3e2f76aa5338c2c48963af41b1a075187925a9
Luverduck <hyekko92@gmail.com>
2024-06-06 17:37:49 +0900

    [27866번] 문자와 문자열

---
+++ 01_by_level/05_string/01_27866.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string str;
+    int idx;
+    cin >> str;
+    cin >> idx;
+
+    cout << str[idx - 1];
+}
