f4959bb394bc9d0e493344cf700512bfad550674
Luverduck <hyekko92@gmail.com>
2024-06-06 17:45:36 +0900

    [2743번] 단어 길이 재기

---
+++ 01_by_level/05_string/02_2743.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    char c;
+
+    int count = 0;
+    while(cin >> c)
+        count++;
+
+    cout << count;
+}
