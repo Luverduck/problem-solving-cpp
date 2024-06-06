fe8b8420e5d2b663f59f478ac53649bc13e0091a
Luverduck <hyekko92@gmail.com>
2024-06-06 18:06:58 +0900

    [11654번] 아스키 코드

---
+++ 01_by_level/05_string/04_11654.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    char c;
+    cin >> c;
+
+    cout << static_cast<int>(c) << endl;
+}
