c6bfd66061a0dbb1b98f382194a521d698916bf4
Luverduck <hyekko92@gmail.com>
2024-06-10 22:32:22 +0900

    [11718번] 그대로 출력하기

--- 01_by_level/05_string/11_11718.cpp
+++ 01_by_level/05_string/11_11718.cpp
@@ -15,6 +15,7 @@ int main()
     // 입력 스트림으로부터 읽은 문자를 저장할 변수
     string str;
 
+    // 입력 스트림으로부터 한 줄을 읽고 개행 문자 '\n'를 제외한 문자열을 str에 저장
     while(getline(cin, str))
         cout << str << "\n";
 }
