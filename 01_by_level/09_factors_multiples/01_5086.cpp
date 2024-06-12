1e7740f47f31c7727c918fd9980884977d0d06a0
Luverduck <hyekko92@gmail.com>
2024-06-12 23:34:54 +0900

    [5086번] 배수와 약수

---
+++ 01_by_level/09_factors_multiples/01_5086.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int first, second;
+
+    while(true)
+    {
+        cin >> first >> second;
+
+        if(first == 0 && second == 0)
+            return 0;
+
+        if(second % first == 0)
+            cout << "factor" << "\n";
+        else if(first % second == 0)
+            cout << "multiple" << "\n";
+        else
+            cout << "neither" << "\n";
+    }
+}
