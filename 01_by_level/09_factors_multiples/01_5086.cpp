93fb4c80e71df2578a13605b9d1456f0558f01d0
Luverduck <hyekko92@gmail.com>
2024-06-13 02:48:35 +0900

    [5086번] 배수와 약수

--- 01_by_level/09_factors_multiples/01_5086.cpp
+++ 01_by_level/09_factors_multiples/01_5086.cpp
@@ -1,30 +1,30 @@
-    #include <iostream>
+#include <iostream>
 
-    using namespace std;
+using namespace std;
 
-    int main()
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int first, second;
+
+    while(true)
     {
-    #ifdef LOCAL_DEBUG
-        freopen("input.txt", "r", stdin);
-    #endif
+        cin >> first >> second;
 
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
+        if(first == 0 && second == 0)
+            return 0;
 
-        int first, second;
-
-        while(true)
-        {
-            cin >> first >> second;
-
-            if(first == 0 && second == 0)
-                return 0;
-
-            if(second % first == 0)
-                cout << "factor\n";
-            else if(first % second == 0)
-                cout << "multiple\n";
-            else
-                cout << "neither\n";
-        }
+        if(second % first == 0)
+            cout << "factor\n";
+        else if(first % second == 0)
+            cout << "multiple\n";
+        else
+            cout << "neither\n";
     }
+}
