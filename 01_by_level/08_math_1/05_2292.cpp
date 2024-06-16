2c8c1c7bcadaa89cb55f6284468b17848d3a67c4
Luverduck <hyekko92@gmail.com>
2024-06-16 21:01:23 +0900

    [2720번] 세탁소 사장 동혁

---
+++ 01_by_level/08_math_1/05_2292.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 목적지 방 번호
+    int N;
+    cin >> N;
+
+    // 현재 층 수 (1번방이 위치한 층을 1층으로)
+    int level = 1;
+    // 현재 층 수에서 가장 큰 방 번호
+    int max = 1;
+
+    // 목적지 방 번호가 현재 층 수에서 가장 큰 방 번호보다 큰 경우
+    while(N > max)
+    {
+        // 다음 층으로 이동
+        max += 6 * level;
+        level++;
+    }
+
+    // 최종 층 수 출력
+    cout << level;
+}
