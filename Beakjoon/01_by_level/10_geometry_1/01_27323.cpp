9fa05c4aa3de97df2f52a997bfe30c55c4db3955
Luverduck <hyekko92@gmail.com>
2024-06-17 21:58:35 +0900

    [27323번] 직사각형

---
+++ Beakjoon/01_by_level/10_geometry_1/01_27323.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << (x * y);
+}
