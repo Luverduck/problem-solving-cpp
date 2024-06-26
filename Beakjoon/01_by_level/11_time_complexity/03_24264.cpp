ef04a1af90610cf58131bfa8ab0e26a868c971dc
Luverduck <hyekko92@gmail.com>
2024-06-26 21:28:45 +0900

    [24264번] 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ Beakjoon/01_by_level/11_time_complexity/03_24264.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 코드 1의 수행 횟수
+long long count = 0;
+
+// 알고리즘
+int MenOfPassion(const vector<int>& vec, const long long n)
+{
+	int sum = 0;
+
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 1; j <= n; ++j)
+        {
+            sum = sum + vec[i] * vec[i];
+            count++;
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+
+    vector<int> vec(n);
+    fill(vec.begin(), vec.end(), 1);
+
+    MenOfPassion(vec, n);
+
+    cout << count << "\n2";
+}
