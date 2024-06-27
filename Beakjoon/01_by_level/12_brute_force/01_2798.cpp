1fa43cac1611c2dad65c79176a55c34e6c9ef5ff
Luverduck <hyekko92@gmail.com>
2024-06-27 22:26:04 +0900

    [2798번] 블랙잭

--- Beakjoon/01_by_level/12_brute_force/01_2798.cpp
+++ Beakjoon/01_by_level/12_brute_force/01_2798.cpp
@@ -25,7 +25,7 @@ int main()
     // 카드 3장의 숫자 합의 최대값 초기화
     int sum_max = -1;
     // 카드 3장의 숫자 합의 임시값 초기화
-    int temp = 0;
+    int sum_temp = 0;
     // 목표값인 M을 넘지 않으며 M에 최대한 가까운 카드 3장의 숫자 합 탐색
     for(int i = 0; i < N - 2; ++i)
     {
@@ -34,12 +34,12 @@ int main()
             for(int k = j + 1; k < N; ++k)
             {
                 // 카드 3장의 숫자 합 계산
-                temp = cards[i] + cards[j] + cards[k];
+                sum_temp = cards[i] + cards[j] + cards[k];
                 // 숫자 합이 M을 넘지 않으며 이전에 갱신한 값보다 큰 경우
-                if(temp <= M && temp > sum_max)
+                if(sum_temp <= M && sum_temp > sum_max)
                 {
                     // 숫자 합의 최대값 갱신
-                    sum_max = temp;
+                    sum_max = sum_temp;
                     // 숫자 합이 목표값과 같을 경우 루프 종료
                     if(sum_max == M) break;
                 }
