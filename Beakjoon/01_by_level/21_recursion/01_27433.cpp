1a08679b3fa2883d1352b1a71e251e9e95efc6d5
Luverduck <hyekko92@gmail.com>
2024-07-20 23:33:41 +0900

    [27433번] 팩토리얼 2

--- Beakjoon/01_by_level/21_recursion/01_27433.cpp
+++ Beakjoon/01_by_level/21_recursion/01_27433.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-long long factorial(long long n)
+// Factorial을 계산할 함수
+long long factorial(long long n) // ⭐ int일 경우 계산 중 최대 표현 범위를 초과할 수 있음
 {
     if(n > 1)
         return factorial(n - 1) * n;
@@ -20,10 +21,17 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+	// 입력
     int n;
     cin >> n;
 
+	// factorial 계산
     long long ret = factorial(n);
+	// ✅ 재귀 호출
+	// N! = N * (N-1)!
+	//	  = N * (N-1) * (N-2)!
+	//    = ...
 
+	// 결과값 출력
     cout << ret;
 }
