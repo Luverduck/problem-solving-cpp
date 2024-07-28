3b4f5b087cada24683277f10e1aac499b9978737
Luverduck <hyekko92@gmail.com>
2024-07-28 20:22:46 +0900

    [10872번] 팩토리얼

--- Beakjoon/01_by_level/19_combination/03_10872.cpp
+++ Beakjoon/01_by_level/19_combination/03_10872.cpp
@@ -12,12 +12,15 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    // 입력 정수 N
     int N;
     cin >> N;
 
+    // N! = N * (N-1) * (N-2) * ... * 3 * 2 * 1
     long long result = 1;
     for(int i = 2; i <= N; ++i)
         result *= i;
 
+    // N! 결과값 출력
     cout << result;
 }
