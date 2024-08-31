827ee3d99a2dc5e01fdf6cf6e830bbe8e063dae9
Luverduck <hyekko92@gmail.com>
2024-08-31 20:31:32 +0900

    [1920번] 수 찾기

--- Beakjoon/01_by_level/27_binary_search/01_1920/main.cpp
+++ Beakjoon/01_by_level/27_binary_search/01_1920/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -13,22 +14,29 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    // 정수의 개수 N
     int N;
     cin >> N;
 
-    int nums[N] { 0, };
+    // 길이 N인 벡터 초기화
+    vector<int> nums(N);
+    // N개의 정수를 벡터에 저장
     for(int i = 0; i < N; ++i)
         cin >> nums[i];
 
-    sort(nums, nums + N);
+    // 벡터 정렬
+    sort(nums.begin(), nums.end());
 
+    // 찾을 정수의 개수 M
     int M;
     cin >> M;
 
+    // 입력받은 M개의 정수 각각이 벡터에 존재하는지 이진 탐색
     int temp;
     for(int i = 0; i < M; ++i)
     {
         cin >> temp;
-        cout << (binary_search(nums, nums + N, temp) ? 1 : 0) << "\n";
+        // 벡터에 해당 정수가 존재하면 1, 그렇지 않으면 0을 출력
+        cout << (binary_search(nums.begin(), nums.end(), temp) ? 1 : 0) << "\n";
     }
 }
