c4bb9ee28f2aa7c71c2c483aae5a578e84f9ab7c
Luverduck <hyekko92@gmail.com>
2024-10-16 21:56:06 +0900

    [9653번] 스타워즈 로고

---
+++ Beakjoon/00_by_ID/09653.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // 로우 문자열 리터럴 (Raw String Literal)
+    cout <<
+        R"(    8888888888  888    88888
+   88     88   88 88   88  88
+    8888  88  88   88  88888
+       88 88 888888888 88   88
+88888888  88 88     88 88    888888
+
+88  88  88   888    88888    888888
+88  88  88  88 88   88  88  88
+88 8888 88 88   88  88888    8888
+ 888  888 888888888 88  88      88
+  88  88  88     88 88   88888888)";
+}
