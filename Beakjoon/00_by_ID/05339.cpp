5d10459db56beb823e6951e6de56abff0685a819
Luverduck <hyekko92@gmail.com>
2024-11-13 10:16:02 +0900

    [05339번] 콜센터

---
+++ Beakjoon/00_by_ID/05339.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << R"(     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+  ||  \ /  ||
+============
+|          |
+|          |
+|          |)";
+
+}
