be3aea38a608626637c252cd5f122f2c4cc915ee
Luverduck <hyekko92@gmail.com>
2024-11-20 19:12:18 +0900

    [16394번] 홍익대학교

---
+++ Beakjoon/00_by_ID/16394.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    cout << (N - 1946);
+}
