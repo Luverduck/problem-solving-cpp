1113a8acfd0cfd15880c1db1930bd35696112b09
Luverduck <hyekko92@gmail.com>
2024-12-12 01:35:19 +0900

    [3733번] Shares

---
+++ Beakjoon/00_by_ID/id_03000_03999/03733.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, S;
+    while (cin >> N >> S)
+        cout << S / (N + 1) << '\n';
+}
