ecc3f0bf354cb4e7e88e1b6e2f19e519e7897a23
Luverduck <hyekko92@gmail.com>
2025-01-25 01:33:54 +0900

    [27959번] 초코바

---
+++ Beakjoon/00_by_ID/id_27000_27999/27959.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    cout << (100 * N >= M ? "Yes" : "No");
+}
