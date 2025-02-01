9f85ca4b020bb2c8c5e0cbd4feb876378325f23f
Luverduck <hyekko92@gmail.com>
2025-02-02 03:27:57 +0900

    [1001번] A-B

---
+++ Beakjoon/00_by_ID/id_01000_01999/01001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A - B;
+}
