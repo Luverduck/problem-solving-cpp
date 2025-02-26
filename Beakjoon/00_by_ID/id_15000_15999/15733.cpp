19b17a78d8d1860042ff7876395611718584b046
Luverduck <hyekko92@gmail.com>
2025-02-27 02:04:55 +0900

    [15733번] 나는 누구인가

---
+++ Beakjoon/00_by_ID/id_15000_15999/15733.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    cout << "I'm Sexy";
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
