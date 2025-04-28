aa30c8b43365239c5026b5aad7661bc6cbbe8a74
Luverduck <hyekko92@gmail.com>
2025-04-29 00:09:41 +0900

    [24078번] 余り (Remainder)

---
+++ baekjoon/00_by_ID/id_24000_24999/24078.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    int X;
+    cin >> X;
+    cout << X % 21;
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
