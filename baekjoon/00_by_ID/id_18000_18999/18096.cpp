41bdf6504bec40038b22da024131515156ffcb6d
Luverduck <hyekko92@gmail.com>
2025-05-01 01:13:54 +0900

    [18096번] Арифметическая магия

---
+++ baekjoon/00_by_ID/id_18000_18999/18096.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    cout << 1;
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
