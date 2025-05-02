4f23323748d7f8eb1fb1cbb550c9caf19695c0fc
Luverduck <hyekko92@gmail.com>
2025-05-03 06:36:01 +0900

    [18409번] 母音を数える (Counting Vowels)

--- baekjoon/00_by_ID/id_18000_18999/18409.cpp
+++ baekjoon/00_by_ID/id_18000_18999/18409.cpp
@@ -1,3 +1,28 @@
-//
-// Created by hyeul on 2025-05-03.
-//
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    int N;
+    string S;
+    cin >> N >> S;
+    int res = 0;
+    for(int n = 0; n < N; ++n) {
+        if(S[n] == 'a' || S[n] == 'i' || S[n] == 'u' || S[n] == 'e' || S[n] == 'o')
+            res++;
+    }
+    cout << res;
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
