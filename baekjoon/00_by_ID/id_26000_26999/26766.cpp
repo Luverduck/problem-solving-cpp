b88df60a69b0438072e23dd7558b6cc812017421
Luverduck <hyekko92@gmail.com>
2025-05-16 02:18:21 +0900

    [26766번] Serca

---
+++ baekjoon/00_by_ID/id_26000_26999/26766.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+    int N;
+    cin >> N;
+    while (N--)
+    {
+        cout << " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n";
+    }
+}
+
+int main()
+{
+#ifdef LOCAL_DEBUG
+    freopen("input.txt", "r", stdin);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solution();
+}
